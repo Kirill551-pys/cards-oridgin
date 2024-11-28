60f2a1868c531550b89e374f717e2d4275951e11
Kirill <taranyuk31@mail.ru>
2024-11-28 23:25:07 +0400

    cheange commit

--- index.js
+++ index.js
@@ -19,29 +19,4 @@ document.addEventListener('DOMContentLoaded', () => {
             servicesModal.style.display = 'none';
         }
     });
-
-    const contacts = [
-        {
-            name: "Алина Бекер",
-            phone: "748293944",
-            email: "ydydtakd@maulmr"
-        },
-    ];
-
-    document.getElementById('exportVCard').addEventListener('click', function() {
-        const vCard = new VCard();
-
-        contacts.forEach(contact => {
-            vCard.addName(contact.name);
-            vCard.addTelephone(contact.phone);
-            vCard.addEmail(contact.email);
-        });
-
-        const vCardData = vCard.toString();
-        const blob = new Blob([vCardData], { type: 'text/vcard' });
-        const link = document.createElement('a');
-        link.href = URL.createObjectURL(blob);
-        link.download = 'contacts.vcf';
-        link.click();
-    });
 });
